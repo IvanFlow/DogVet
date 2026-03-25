2d428eb1c002ff01f2d7ccd7f23e9309ac3cbc13
IvanApex <ivflores@ApexSystems.net>
2026-03-24 22:25:24 -0600

    feat: migration to remove Pets.Age column

---
+++ DogVetAPI.Data/Migrations/20260325041543_RemovingAgeFromPets.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DogVetAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovingAgeFromPets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Pets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Pets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
